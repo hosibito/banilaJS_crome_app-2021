2a693685f27d5efa88f1b7a6038d9cc1702013c4
hosibito <hosibito@naver.com>
2022-03-07 23:00:06 +0900

    5.2 PadStart(앞에 0 추가!)

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,15 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
 setInterval(getClock, 1000);  
 
-//setTimeout(getClock, 1000); 지정된 시간후 한번만 실행
+//"1".padStart(3,"0")  => 문자열이 지정된거보다 작으면 앞에 지정된 문자열을 추가한다.
+//             2 보다 작으면 앞에 0 을 추가한다.   =>  "002" 가 된다. 
+//"1".padEnd(3,"0")  => "200" 이된다. 
