450b3f7b5fa8eba9ed160694de3d8622c68e2890
hosibito <hosibito@naver.com>
2022-03-07 22:04:53 +0900

    4.4 Getting Username

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput= document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
-    event.preventDefault();  // 서브밋 클릭시 데이터 전송하고 새로고침 해야하나 막았다.
-    console.log(event);
-}
-
-function handleLinkClick(event) {
-    event.preventDefault();  // 링크클릭시 링크로 가야하나 막았다.
-    console.log(event);    
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`; // python f"Hello {username}""
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
-link.addEventListener("click", handleLinkClick)
+// 입력 form 에 이름을 입력하면 폼이 사라지고
+// h1 태그안에 Hello 유저네임  이 나타난다. 
 
-// 1. form에서 submit 가 들어가면 
-//     기본 - 폼 데이터를 전송하고 새로고침을한다.
-// 2. EventListener 는 기본적으로 이벤트에 대한 정보를 제공한다.
-//     이는 function 의 첫번째 인자로 전달돤다. (생략가능, 생략하면 안받는다. )
-//     이 이벤트의 대한 정보로 조작이 가능하다. (ex : event.preventDefault(); 기본 동작을 막는다. )
