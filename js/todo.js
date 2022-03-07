1f5d6446e430a094170192a78bd75c8707d44566
hosibito <hosibito@naver.com>
2022-03-07 23:54:17 +0900

    7.2 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,25 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  //console.log(event.target)  을 찍어봐라!. 자세한 정보가 나온다. 
+  const li = event.target.parentElement; // 타겟의 부모 엘리멘트를 특정짓는다.(li)
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
-  const span = document.createElement("span");
-  li.appendChild(span);
+  const span = document.createElement("span");  
   span.innerText = newTodo;
+  const button = document.createElement("button");  
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
+//todo 를 받아오면 버튼과 함게 추가한다. (버튼은 이벤트를 연결해준다. )
+
 
 function handleToDoSubmit(event) {
   event.preventDefault();
@@ -19,5 +31,3 @@ function handleToDoSubmit(event) {
 
 toDoForm.addEventListener("submit", handleToDoSubmit);  
 
-// todo  입력을 받아서
-// <li><span>todo입력내용</span></li> 를 만들어서  문서안의 ul 안에 추가한다.
