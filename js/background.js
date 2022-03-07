4b6ef6a17bd2b145917f68104e88c31b77f9d1bb
hosibito <hosibito@naver.com>
2022-03-07 23:25:28 +0900

    6.1 Background(js 로 태그 추가하기)

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");  // <img> 태그가 만들어진다. 
+
+bgImage.src = `img/${chosenImage}`;  // <img src="img/1.jpg"> 태그에 src 추가한다. 
+
+document.body.appendChild(bgImage);  // body 태그안에 만들어진 태그 마지막에  추가한다. 
+// document.body.prepend(bgImage);  // body 태그안에 만들어진 태그 맨앞에  추가한다. 
