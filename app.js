1cc799e3be5c7b752df7ec8c0032c2987d8e9b86
hosibito <hosibito@naver.com>
2022-03-07 22:28:50 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username); // 로컬 스토리지에 저장!
-  // .setItem("username", username)  .getItem("username")  .removeItem("username")
-  greeting.innerText = `Hello ${username}`; 
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY); //시작시 로컬스토리지에서 username 를 가져온다.
+if (savedUsername === null) {           // 없으면 폼을 보이게한다. 
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {                                // 있으면 h1 을 보이게 한다. 
+    paintGreetings(savedUsername);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
