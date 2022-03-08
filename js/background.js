1faf1d803abbcbec6491102a1cf1b39186e5437f
hosibito <hosibito@naver.com>
2022-03-08 11:53:00 +0900

    끝!

--- js/background.js
+++ js/background.js
@@ -5,6 +5,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");  // <img> 태그가 만들어진다. 
 
 bgImage.src = `img/${chosenImage}`;  // <img src="img/1.jpg"> 태그에 src 추가한다. 
+bgImage.classList.add("bgImage")
 
 document.body.appendChild(bgImage);  // body 태그안에 만들어진 태그 마지막에  추가한다. 
 // document.body.prepend(bgImage);  // body 태그안에 만들어진 태그 맨앞에  추가한다. 
