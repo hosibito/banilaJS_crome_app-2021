b8d9f5c0c49379f39146642b9e5d1f785115f01b
hosibito <hosibito@naver.com>
2022-03-08 11:38:57 +0900

    8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,21 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "90c8e09510d12dc1729d0bbc1bdd7eab";
+
 function onGeoOk(position) { 
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;  
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)  // python requests.get(url)
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
-alert("Can't find you. No weather for you.");
+    alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); 
-// 유저의 위치를 반환.(유저가 허용) 브라우저에서 기본으로 제공된다.
